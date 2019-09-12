0deef9ca79f51a4fd4e6c48bcb9c0b77175d1033
Samuel Gomez <samuelgomezmartin@gmail.com>
2019-09-12 09:32:46 +0200

    changes webpack config mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path')
 
 module.exports = {
+  mode: 'development',
+  // mode: 'production',
   entry: "./src/index.js",
   output: {
-    // path: './dist',
     path: path.join(__dirname, 'dist'),
     filename: 'app.bundle.js'
   }
